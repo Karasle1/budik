cd4bd499f1603ac9c8a42be21a54d15d19281c0d
Leoš Karásek <leos.karasek@seznam.cz>
2019-05-14 16:24:33 +0200

    hodiny

--- src/start2.java
+++ src/start2.java
@@ -1,28 +1,83 @@
 
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.opengl.util.FPSAnimator;
 
-import java.awt.Frame;
-import javax.swing.JFrame;
 
-import static com.jogamp.opengl.math.FloatUtil.sin;
-import static com.jogamp.opengl.math.FloatUtil.cos;
+import java.awt.*;
+import java.time.LocalDateTime;
+
 
 
 public class start2 implements GLEventListener {
+    private GLU glu = new GLU();
 
     @Override
     public void display(GLAutoDrawable drawable) {
-        final GL2 gl = drawable.getGL().getGL2();
 
-        gl.glBegin(GL.GL_LINE_LOOP);
-        for(int i =0; i <= 300; i++){
-            double angle = 2 * Math.PI * i / 300;
+        final GL2 gl = drawable.getGL().getGL2();
+        gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL3.GL_DEPTH_BUFFER_BIT );
+
+
+        gl.glBegin(GL2.GL_TRIANGLE_FAN);
+        gl.glVertex3d(0.00f,0.00f,5.0f);
+       for(int i =0; i <= 360; i++){
+            double angle = 2 * Math.PI * i / 360;
             double x = Math.cos(angle);
             double y = Math.sin(angle);
-            gl.glVertex2d(x,y);
+            gl.glColor3f(0.09f, 0.09f, 0.09f );
+            gl.glVertex3d(x,y,5.0f);
+            gl.glVertex3d(x,y,5.0f);
+        }
+       gl.glEnd();
+
+        gl.glBegin(GL2.GL_TRIANGLE_FAN);
+        gl.glVertex3d(0.0f,0.0f,-5f);
+        for(int i =0; i <= 360; i++){
+            double angle = 2 * Math.PI * i / 360;
+            double x = Math.cos(angle);
+            double y = Math.sin(angle);
+            gl.glColor3f(0.9f, 0.09f, 0.09f );
+            gl.glVertex3d(x,y,-5f);
+            gl.glVertex3d(x,y,-5f);
+
         }
         gl.glEnd();
+
+
+        int hour = LocalDateTime.now().getHour();
+        float deltaH = -(hour * 30);
+        gl.glLoadIdentity();
+        gl.glRotatef(deltaH,0,0,1);
+        gl.glBegin(GL2.GL_TRIANGLES);
+        gl.glColor3f(1.0f, 0.0f, 0.0f );
+        gl.glVertex3f(-0.010f,0.00f,-5.0f);
+        gl.glVertex3f(0.010f,0.00f,-5.0f);
+        gl.glVertex3f(0.00f,0.50f,-5.0f);
+        gl.glEnd();
+
+        int minute = LocalDateTime.now().getMinute();
+        float deltaM = -(minute * 6);
+        gl.glLoadIdentity();
+        gl.glRotatef(deltaM,0,0,1);
+        gl.glBegin(GL2.GL_TRIANGLES);
+        gl.glColor3f(0.0f,1.0f,0.0f);
+        gl.glVertex3f(-0.01f,0.0f,-5.0f);
+        gl.glVertex3f(0.01f,0.0f,-5.0f);
+        gl.glVertex3f(0.0f,0.70f,-5.0f);
+        gl.glEnd();
+
+        int sec = LocalDateTime.now().getSecond();
+        float deltaS = -(sec * 6);
+        gl.glLoadIdentity();
+        gl.glRotatef(deltaS,0,0,1);
+        gl.glBegin(GL2.GL_LINE_STRIP);
+        gl.glColor3f(0.0f,0.50f,-5.50f);
+        gl.glVertex3f(0.0f,0.0f,-5.0f);
+        gl.glVertex3f(0.0f,0.80f,-5.0f);
+        gl.glEnd();
+
     }
     @Override
     public void dispose(GLAutoDrawable arg0) {
@@ -35,8 +90,20 @@ public class start2 implements GLEventListener {
     }
 
     @Override
-    public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {
-        // method body
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        GL2 gl = drawable.getGL().getGL2();
+
+        if( height <= 0 )
+            height = 1;
+
+        final float h = ( float ) width / ( float ) height;
+        gl.glViewport( 0, 0, width, height );
+        gl.glMatrixMode( GL2.GL_PROJECTION );
+        gl.glLoadIdentity();
+
+        glu.gluPerspective( 45.0f, h, 1.0, 20.0 );
+        gl.glMatrixMode( GL2.GL_MODELVIEW );
+        gl.glLoadIdentity();
     }
 
     public static void main(String[] args) {
@@ -56,8 +123,10 @@ public class start2 implements GLEventListener {
 
         //adding canvas to frame
         frame.add(glcanvas);
-        frame.setSize( 800, 600 );
+        frame.setSize( 1024, 780 );
         frame.setVisible(true);
+        final FPSAnimator animator = new FPSAnimator(glcanvas, 1,true);
+        animator.start();
     }
 
 }
