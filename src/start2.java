5f645820cd541096e75650ec45c93e246cf8e715
Leoš Karásek <leos.karasek@seznam.cz>
2019-06-21 14:59:13 +0200

    buzeni nataveni

--- src/start2.java
+++ src/start2.java
@@ -14,7 +14,6 @@ import sun.audio.AudioStream;
 import transforms.Camera;
 import transforms.Vec3D;
 
-
 import static com.jogamp.opengl.GL.*;
 
 public class start2 extends Thread implements GLEventListener, MouseListener,
@@ -24,17 +23,17 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
     Camera cam = new Camera().withPosition(new Vec3D(0, 0, -1.5))
             .withAzimuth(Math.PI * 1.25)
             .withZenith(Math.PI * -0.125);
-    int ox, oy, delta, buzeniX, buzeniY;
-    int width = 100;
-
+    int ox, oy, delta,width = 100 ;
+    double buzeniX, buzeniY;
 
     Texture texture,texture0,texture1;
-
-    IntBuffer textId = IntBuffer.allocate(10);
 
     ArrayList cifernik = new ArrayList<Vec3D>();
     ArrayList zada = new ArrayList<Vec3D>();
     boolean wakeTime;
+    double deltaBuzeni;
+    int hour = LocalDateTime.now().getHour();
+    double deltaH = -(hour * 30)  - Math.abs(LocalDateTime.now().getMinute() / 2);
 
     @Override
     public void display(GLAutoDrawable drawable) {
@@ -43,22 +42,12 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
         gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL3.GL_DEPTH_BUFFER_BIT);
         gl.glEnable(GL2GL3.GL_DEPTH_TEST);
 
-        int hour = LocalDateTime.now().getHour();
-        float deltaH = -(hour * 30);
-        int minute = LocalDateTime.now().getMinute();
-        float deltaM = -(minute * 6);
-        int sec = LocalDateTime.now().getSecond();
-        float deltaS = -(sec * 6);
-
-
-
-        double xc = cam.getPosition().getX();
-        double yc = cam.getPosition().getY();
-        double zc = cam.getPosition().getZ();
+        int  minute = LocalDateTime.now().getMinute(),sec = LocalDateTime.now().getSecond();
+         // overit ????
+        float deltaM = -(minute * 6), deltaS = -(sec * 6);
+        double xc = cam.getPosition().getX(),yc = cam.getPosition().getY(),zc = cam.getPosition().getZ();
         GLUquadric quadObj;
-
         quadObj = glu.gluNewQuadric();
-
 
         float[] light_position = new float[]{1, 0, 0, 1.0f};
 
@@ -150,25 +139,19 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
         gl.glTexEnvi(gl.GL_TEXTURE_ENV, gl.GL_TEXTURE_ENV_MODE, gl.GL_REPLACE);
         gl.glBegin(GL2.GL_QUAD_STRIP);
         for (int i = 0; i <= 360; i++) {
-
             Vec3D a = (Vec3D) cifernik.get(i);
             gl.glTexCoord2d(a.getX(), a.getY());
-     //       gl.glColor3f(0.09f, 0.09f, 0.9f);
             gl.glVertex3d(a.getX(), a.getY(), a.getZ());
-
             Vec3D b = (Vec3D) zada.get(i);
             gl.glTexCoord2d(b.getX(), b.getY());
-      //      gl.glColor3f(0.09f, 0.09f, 0.9f);
             gl.glVertex3d(b.getX(), b.getY(), b.getZ());
         }
-
         gl.glEnd();
 
 // rucicky
         camera(gl, xc, yc, zc);
-        gl.glRotatef(deltaH, 0, 0, 1);
+        gl.glRotated(deltaH, 0, 0, 1);
         gl.glBegin(GL2.GL_TRIANGLES);
-    //    gl.glColor3f(1.0f, 0.0f, 0.0f);
         gl.glVertex3f(-0.030f, 0.00f, -3.9f);
         gl.glVertex3f(0.030f, 0.00f, -3.9f);
         gl.glVertex3f(0.00f, 0.50f, -3.9f);
@@ -177,7 +160,6 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
         camera(gl, xc, yc, zc);
         gl.glRotatef(deltaM, 0, 0, 1);
         gl.glBegin(GL2.GL_TRIANGLES);
-   //    gl.glColor3f(0.0f, 1.0f, 0.0f);
         gl.glVertex3f(-0.03f, 0.0f, -3.9f);
         gl.glVertex3f(0.03f, 0.0f, -3.9f);
         gl.glVertex3f(0.0f, 0.70f, -3.9f);
@@ -186,16 +168,14 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
         camera(gl, xc, yc, zc);
         gl.glRotatef(deltaS, 0, 0, 1);
         gl.glBegin(GL2.GL_LINE_STRIP);
-   //     gl.glColor3f(0.02f, 0.50f, -3.90f);
         gl.glVertex3f(0.0f, 0.0f, -3.90f);
         gl.glVertex3f(0.0f, 0.80f, -3.90f);
         gl.glEnd();
 
         camera(gl, xc, yc, zc);
-      //  gl.glRotatef(deltaS, 0, 0, 1);
+        gl.glRotated(deltaBuzeni, 0, 0, 1);
         texture1.bind(gl);
         gl.glBegin(GL2.GL_QUAD_STRIP);
-     //   gl.glColor3f(0.255f, 0.0f, 0.0f);
         gl.glVertex3f(-0.01f, 0.0f, -3.90f);
         gl.glVertex3f(0.01f, 0.0f, -3.90f);
         gl.glVertex3f(-0.01f, 0.8f, -3.90f);
@@ -204,8 +184,19 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
         texture0.bind(gl);
 
         //palicka
-        wakeTime = true;
+      System.out.println((Math.abs(Math.round(deltaBuzeni*10))));
+        System.out.println(Math.abs((int)deltaH));
+        if ( (Math.abs(Math.round(deltaBuzeni*10)) < Math.abs((int)deltaH - 10 )+70)  && (Math.abs(Math.round(deltaBuzeni*10)) > Math.abs((int)deltaH + 10)+70)  ) //&& )
 
+
+        {
+            wakeTime = true;
+            System.out.println(Math.round(deltaBuzeni * 10));
+        }
+        else
+        {
+            wakeTime = false;
+        }
         if (!wakeTime) {
             camera(gl, xc, yc, zc);
             gl.glTranslatef(0f, 1.5f, -4.5f);
@@ -222,13 +213,11 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
            long mili = System.currentTimeMillis();
                 zvoneni(gl, quadObj, xc, yc, zc, mili);
 
-                zvuk();
+            //    zvuk();
 
 
             }
         }
-
-
     private void camera(GL2 gl, double xc, double yc, double zc) {
         gl.glLoadIdentity();
         gl.glRotated(-cam.getAzimuth(), 0.0f, 1.0f, 0.0f);
@@ -237,8 +226,7 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
         gl.glTranslated(xc, yc, zc);
     }
 
-
-    private void zvuk() {
+  /*  private void zvuk() {
 
         Thread z = new Thread();
         public void run() {
@@ -256,10 +244,9 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
 
 
         }
-    }
+    } */
 
     private void zvoneni(GL2 gl,GLUquadric quadObj,double xc, double yc, double zc, long mili) {
-
 
                  if ((mili) % 2 != 0) {
                     camera(gl, xc, yc, zc);
@@ -272,7 +259,6 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
                      gl.glRotated(90, 0.0f, 1.0f, 0.0f);
                      gl.glRotated(160 , 1.0f, 0.0f, 0.0f);
                      glu.gluCylinder(quadObj, 0.05f, 0.05f, 0.3f, 32, 32);
-
                 }
                 else if ((mili) % 2 == 0){
                     camera(gl, xc, yc, zc);
@@ -286,12 +272,7 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
                      gl.glRotated(200, 1.0f, 0.0f, 0.0f);
                      glu.gluCylinder(quadObj, 0.05f, 0.05f, 0.3f, 32, 32);
             }
-
         }
-
-
-
-
     @Override
     public void dispose(GLAutoDrawable glDrawable) {
 
@@ -302,28 +283,20 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
     @Override
     public void init(GLAutoDrawable drawable) {
         final GL2 gl = drawable.getGL().getGL2();
-     //   BufferedImage img = new BufferedImage (500,500,TYPE_BYTE_BINARY);
+
         gl.glMatrixMode(GL2.GL_PROJECTION);
-                gl.glLoadIdentity();
+        gl.glLoadIdentity();
         gl.glEnable(GL2GL3.GL_DEPTH_TEST);
         gl.glFrustum(-1.0, 1.0, -1.0, 1.0, 3.0, 500.0);
-        //gl.glActiveTexture(gl.GL_TEXTURE0);
        try {
-            // System.out.print("Loading texture...");
+
             texture = TextureIO.newTexture(new File("textures/ciselnik.png"), true);
-       //     texture.bind(gl);
             texture0 = TextureIO.newTexture(new File("textures/mosaz1.jpg"), true);
-         //   texture0.bind(gl);
-           texture1 = TextureIO.newTexture(new File("textures/textureRed.jpg"), true);
+            texture1 = TextureIO.newTexture(new File("textures/textureRed.jpg"), true);
+           } catch (IOException e) {
 
-
-         //   img = ImageIO.read(new File("textures/desk.jpg"));
-            //    System.out.println("ok");
-        } catch (IOException e) {
-            //   System.out.println("failed");
             System.out.println(e);
         }
-
         gl.glTexParameteri(GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR);
         gl.glTexParameteri(GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST);
         gl.glEnable(GL_TEXTURE_2D);
@@ -346,8 +319,6 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
         gl.glMatrixMode( GL2.GL_MODELVIEW );
         gl.glLoadIdentity();
     }
-
-
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -403,7 +374,6 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
 
         buzeniX = e.getX();
         buzeniY = e.getY();
-
     }
 
     @Override
@@ -429,10 +399,23 @@ public class start2 extends Thread implements GLEventListener, MouseListener,
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        cam = cam.addAzimuth( Math.PI * (ox - e.getX()) / width)
-                .addZenith( Math.PI * (e.getY() - oy) / width);
-        ox = e.getX();
-        oy = e.getY();
+
+
+        if ((ox < 400 || ox > 700) || (oy < 300 || oy > 700)) {
+            cam = cam.addAzimuth(Math.PI * (ox - e.getX()) / width)
+                    .addZenith(Math.PI * (e.getY() - oy) / width);
+            ox = e.getX();
+            oy = e.getY();
+
+        } else {
+            buzeniX = e.getX();
+            buzeniY = e.getY();
+            buzeniX = (buzeniX/1000 -0.5);
+            buzeniY = -(buzeniY/1000 -0.5);
+            deltaBuzeni = - Math.toDegrees(Math.atan2(buzeniX, buzeniY));
+         //   System.out.println(deltaBuzeni);
+          //  System.out.println(deltaH);
+        }
     }
 
     @Override
